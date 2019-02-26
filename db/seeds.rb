d55d7a1925a30999cbb10e7784beee29de6fc8e1
Nicolas Bocquet <nicolas.fow@gmail.com>
2019-02-26 16:32:33 +0100

    Branche en attente : db non finalisée

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  user = User.new(
+    first_name: Faker::Name.unique.first_name,
+    last_name: Faker::Name.last_name,
+    agency: Faker::Company.unique.name,
+    agency_address: Faker::Address.full_address,
+    email: Faker::Internet.email,
+    password: "password"
+    )
+  user.save!
+  beneficiary = Beneficiary.new(user: user)
+end
+
+10.times do
+  beneficiary = Beneficiary.new(
+    )
+
+puts 'Finished!'

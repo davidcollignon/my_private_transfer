bbdc8e9a04be992bfecf40a6bdc2309c84961c6a
Lise Gastaldon <gastaldon.lise@orange.fr>
2019-02-27 10:48:54 +0100

    home_and_cars-index-en-cours

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,68 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_151219) do
+ActiveRecord::Schema.define(version: 2019_02_25_162621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "beneficiaries", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "title"
+    t.string "language"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cars", force: :cascade do |t|
+    t.bigint "company_id"
+    t.string "brand"
+    t.text "description"
+    t.integer "passenger_capacity"
+    t.integer "luggage_capacity"
+    t.float "price_per_hour"
+    t.float "price_per_km"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_cars_on_company_id"
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.bigint "car_id"
+    t.bigint "user_id"
+    t.bigint "beneficiary_id"
+    t.datetime "pick_up_date"
+    t.float "final_price"
+    t.string "status"
+    t.string "flight_number"
+    t.integer "number_of_passengers"
+    t.integer "number_normal_luggage"
+    t.integer "number_odd_luggage"
+    t.text "description_odd_luggage"
+    t.integer "number_hand_luggage"
+    t.string "additional_info"
+    t.float "commission_rate"
+    t.string "service_type"
+    t.string "pick_up_address"
+    t.string "destination_address"
+    t.float "number_hours_at_disposal"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["beneficiary_id"], name: "index_services_on_beneficiary_id"
+    t.index ["car_id"], name: "index_services_on_car_id"
+    t.index ["user_id"], name: "index_services_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +81,19 @@ ActiveRecord::Schema.define(version: 2019_02_25_151219) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "beneficiary_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "agency"
+    t.string "agency_address"
+    t.index ["beneficiary_id"], name: "index_users_on_beneficiary_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cars", "companies"
+  add_foreign_key "services", "beneficiaries"
+  add_foreign_key "services", "cars"
+  add_foreign_key "services", "users"
+  add_foreign_key "users", "beneficiaries"
 end

2734ae238db385af768bf198217b6cf320fc51ca
Nicolas Bocquet <nicolas.fow@gmail.com>
2019-02-28 11:12:28 +0100

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :generate_css_class
+
+  def generate_css_class
+    @cssclass = params[:action]
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
 
-  layout "home", only: [:home]
-
   skip_before_action :authenticate_user!, only: [:home, :index]
 
   def home

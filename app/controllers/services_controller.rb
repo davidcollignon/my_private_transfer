642cb63c160368809ec0318cc3498302b03cbc8c
Lise Gastaldon <gastaldon.lise@orange.fr>
2019-02-28 14:37:01 +0100

    edit_update_delete for service are done

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -20,20 +20,35 @@ class ServicesController < ApplicationController
     @hours = params[:service][:number_hours_at_disposal]
     @commission_rate = params[:service][:commission_rate]
     @amount = @car.price_per_hour_cents * @hours.to_i * (1 + @commission_rate.to_f)
-    @service = Service.create!(car: @car, final_price: @amount, status: 'pending', user: current_user)
+    @service = Service.new(service_params)
+    @service.car = @car
+    @service.final_price = @amount
+    @service.status = "pending"
+    @service.user = current_user
+    @service.save!
+    #create!(service_params, car: @car, final_price: @amount, status: 'pending', user: current_user)
     redirect_to new_service_payment_path(@service)
   end
 
   def edit
-
     @service = Service.find(params[:id])
+    @service.car
   end
 
   def update
-
     @service = Service.find(params[:id])
+    @car = @service.car
+   #@car = Car.find(params[:service][:car])
+    @hours = params[:service][:number_hours_at_disposal]
+    @commission_rate = params[:service][:commission_rate]
+    @amount = @car.price_per_hour_cents * @hours.to_i * (1 + @commission_rate.to_f)
+    @service = Service.find(params[:id])
+    @service.car = @car
+    @service.final_price = @amount
+    @service.status = "pending"
+    @service.user = current_user
     if @service.update(service_params)
-    redirect_to edit_service_path(@service)
+    redirect_to new_service_payment_path(@service)
     else
       render :edit
     end
@@ -41,6 +56,7 @@ class ServicesController < ApplicationController
 
   def destroy
     @service = Service.find(params[:id])
+    @service.car
     @service.delete
     redirect_to root_path
   end
@@ -48,7 +64,8 @@ class ServicesController < ApplicationController
   private
 
   def service_params
-    #params.require(:service).permit(:flight_number, :number_hours_at_disposal, :number_of_passengers, :number_normal_luggage, :number_hand_luggage, :number_odd_luggage, :description_odd_luggage, :additional_info, :commission_rate, :car, :user, :pick_up_date, :final_price, :status, :payment, :driver_language, :service_type, :pick_up_address, :destination_address)
-  params.permit(:data)
+   # params.require(:service).permit(:flight_number, :number_hours_at_disposal, :number_of_passengers, :number_normal_luggage, :number_hand_luggage, :number_odd_luggage, :description_odd_luggage, :additional_info, :commission_rate, :car, :user, :pick_up_date, :final_price, :status, :payment, :driver_language, :service_type, :pick_up_address, :destination_address)
+  # params.permit(:data)
+  params.require(:service).permit(:final_price, :flight_number, :driver_language, :number_hours_at_disposal, :number_of_passengers, :number_normal_luggage, :number_hand_luggage, :number_odd_luggage, :description_odd_luggage, :additional_info, :commission_rate)
   end
 end

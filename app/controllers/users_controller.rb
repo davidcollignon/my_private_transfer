6159679a9fcb995c796f4092d4026be63f0077c0
David Collignon <david.s.collignon@gmail.com>
2019-02-28 11:36:21 +0100

    dashboard backend completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,11 +28,21 @@ class UsersController < ApplicationController
     #
     index = 1
     @monthly_revenue = []
-    @monthly_revenue << -1
+    @monthly_comm = []
+    @monthly_potential_revenue = []
+    @monthly_potential_comm = []
 
-    while index < DateTime.now.month + 1 do
+    while index <= DateTime.now.month do
       @monthly_revenue << sum_rev(index)
+      @monthly_comm << sum_comm(index)
+      @monthly_potential_revenue << sum_potential_rev(index)
+      @monthly_potential_comm << sum_potential_comm(index)
+      index +=1
     end
+    @monthly_revenue
+    @monthly_comm
+    @monthly_potential_revenue
+    @monthly_potential_comm
 
 
 
@@ -41,10 +51,41 @@ class UsersController < ApplicationController
   def sum_rev(month)
     @total_sales = 0
     @services.each do |service|
-      if service.pick_up_date.month == month
-        @total_sales += service.final_price_cents
+      if service.pick_up_date.month == month && service.status == "Done"
+        @total_sales += (service.final_price_cents / 100)
       end
     end
     @total_sales
   end
+
+  def sum_comm(month)
+    @total_comm = 0
+    @services.each do |service|
+      if service.pick_up_date.month == month && service.status == "Done"
+        @total_comm += (service.final_price_cents * service.commission_rate / 100)
+      end
+    end
+    @total_comm
+  end
+
+  def sum_potential_rev(month)
+    @total_sales = 0
+    @services.each do |service|
+      if service.pick_up_date.month == month && service.status != "Done"
+        @total_sales += (service.final_price_cents / 100)
+      end
+    end
+    @total_sales
+  end
+
+  def sum_potential_comm(month)
+    @total_comm = 0
+    @services.each do |service|
+      if service.pick_up_date.month == month && service.status != "Done"
+        @total_comm += (service.final_price_cents * service.commission_rate / 100)
+      end
+    end
+    @total_comm
+  end
+
 end

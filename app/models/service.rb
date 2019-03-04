735b5125f1eaa60940ac4e7cad5e8931769b938c
Claudia Wiese <claudia.wiese@sciencespo.fr>
2019-03-04 13:23:37 +0100

    mailing to be continued

--- app/models/service.rb
+++ app/models/service.rb
@@ -4,6 +4,12 @@ class Service < ApplicationRecord
   # belongs_to :beneficiary, optional: true
 
   # validates :pick_up_date, presence: true
+  validates :number_of_passengers, presence: true
+  validates :number_normal_luggage, presence: true
+  validates :number_hand_luggage, presence: true
+  validates :number_odd_luggage, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
 
   monetize :final_price_cents
 end

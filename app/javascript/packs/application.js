b9d63cd71da80571fc9ab5aae9dfebe9af1c6285
David Collignon <david.s.collignon@gmail.com>
2019-03-05 18:23:08 +0100

    slider testimonies almost done, only card to redesign and fake testimonies to write

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,33 @@
 import "bootstrap";
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import flatpickr from "flatpickr";
+import { tns } from "tiny-slider/src/tiny-slider"
+
 
 flatpickr("#pick_up_date", {enableTime: true, weekNumbers: true});
 
 initAutocomplete();
+
+
+const slider = tns({
+    container: '.my-slider',
+    items: 1,
+    slideBy: 'page',
+    nav: false,
+    autoplay: false,
+    speed: 400,
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    lazyload: true,
+    controlsContainer: "#customize-controls",
+    responsive: {
+        100: {
+            items: 1,
+        },
+
+        768: {
+            items: 1,
+        }
+    }
+  });
+

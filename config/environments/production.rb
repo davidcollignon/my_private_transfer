239d206081b558b6be89029a1b2bbff520a7e2f9
Claudia Wiese <claudia.wiese@sciencespo.fr>
2019-03-07 16:23:53 +0100

    fixing mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   #Configure Postmark for production
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
-  config.action_mailer.default_url_options = { host: "myprivatetransfer.vip" }
+  config.action_mailer.default_url_options = { host: "www.myprivatetransfer.vip" }
     # or your custom domain name eg. "www.yourdomain.com"
 
  # config.action_mailer.delivery_method = :smtp
